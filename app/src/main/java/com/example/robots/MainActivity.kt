24fbb47a4db398e9b48ebba2be158c08363174f5
paul-satoru-hernandez <paul.hernandez416@gmail.com>
2023-09-13 18:41:13 -0400

    homework finished

--- app/src/main/java/com/example/robots/MainActivity.kt
+++ app/src/main/java/com/example/robots/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.robots
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +11,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var redImg : ImageView
     private lateinit var whiteImg : ImageView
     private lateinit var yellowImg : ImageView
+    private lateinit var rotateCounter : ImageButton
+    private lateinit var rotateClock : ImageButton
 
-    var turnCount = 0
+    private var turnCount = -10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +24,30 @@ class MainActivity : AppCompatActivity() {
         whiteImg = findViewById(R.id.whiteRobot)
         yellowImg = findViewById(R.id.yellowRobot)
 
-        redImg.setOnClickListener{ toggleImage() }
-        whiteImg.setOnClickListener{ toggleImage() }
-        yellowImg.setOnClickListener{ toggleImage() }
+        rotateClock = findViewById(R.id.rotateClock)
+        rotateCounter = findViewById(R.id.rotateCounter)
+
+        // Has the click listener change turnCount instead of the function
+        rotateClock.setOnClickListener {
+            turnCount++;
+            toggleImage(true)
+        }
+        rotateCounter.setOnClickListener{
+            turnCount--;
+            toggleImage(false)
+        }
     }
 
-    private fun toggleImage() {
-        turnCount++
-        if(turnCount > 2) {
+    private fun toggleImage(vararg rotation: Boolean) {
+        Log.d("Test", "value = $turnCount")
+
+        if (turnCount == -9 || turnCount == -11) {
+            turnCount = 1;
+        }
+        if (turnCount > 2) {
             turnCount = 0
+        } else if (turnCount < 0) {
+            turnCount = 2;
         }
         redImg.setImageResource(R.drawable.king_of_detroit_robot_red_small)
         whiteImg.setImageResource(R.drawable.king_of_detroit_robot_white_small)
@@ -36,12 +55,9 @@ class MainActivity : AppCompatActivity() {
 
         when (turnCount) {
             1 -> redImg.setImageResource(R.drawable.king_of_detroit_robot_red_large);
-            2 -> whiteImg.setImageResource(R.drawable.king_of_detroit_robot_white_large);
-            else -> {
-                yellowImg.setImageResource(R.drawable.king_of_detroit_robot_yellow_large);
-            }
+            2 -> yellowImg.setImageResource(R.drawable.king_of_detroit_robot_yellow_large);
+            0 -> whiteImg.setImageResource(R.drawable.king_of_detroit_robot_white_large);
         }
-
     }
 
 }
